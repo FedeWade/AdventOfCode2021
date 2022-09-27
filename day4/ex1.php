5057fb6b981441de5c03c718d6288e0232d07f68
FedeWade <federico.ballarin.2@studenti.unipd.it>
2022-09-27 17:43:50 +0200

    added day4 ex1

---
+++ day4/ex1.php
@@ -0,0 +1,67 @@
+<?
+$fileName = __DIR__ . "/input.txt";
+$inputFile = fopen($fileName, "r") or die("unable to open file");
+
+$estractions = explode(',', trim(fgets($inputFile)));
+fgets($inputFile);
+
+$boardsArray = array();
+$board = $row = $col = 0;
+
+while (!feof($inputFile)) {
+    $currentLine = trim(str_replace("  ", " ", fgets($inputFile)));
+
+    if ($currentLine == "") {
+        $board++;
+        $row = 0;
+    } else {
+        $currentLine = explode(' ', $currentLine);
+        $col = 0;
+        foreach ($currentLine as $key => $number) {
+            $boardsArray[$board][$row][$col] = $number;
+            $col++;
+        }
+        $row++;
+    }
+}
+
+$winningBoard;
+$winningNumber;
+foreach ($estractions as $key => $estractionNumber) {
+    foreach ($boardsArray as $boardIndex => $board) {
+        foreach ($board as $rowIndex => $row) {
+            foreach ($row as $colIndex => $currBoardNumber) {
+                if ($currBoardNumber == $estractionNumber) {
+                    $boardsArray[$boardIndex][$rowIndex][$colIndex] = "x";
+
+                    $rowCheck = $colCheck = true;
+                    for ($i = 0; $i < 5; $i++) {
+                        if ($boardsArray[$boardIndex][$i][$colIndex] != "x") $rowCheck = false;
+                        if ($boardsArray[$boardIndex][$rowIndex][$i] != "x") $colCheck = false;
+                    }
+                    if ($colCheck || $rowCheck) {
+                        $winningBoard = $boardIndex;
+                        $winningNumber = $estractionNumber;
+                        echo "BINGO! board: " . $boardIndex . PHP_EOL;
+                        break 4;
+                    }
+                }
+            }
+        }
+    }
+}
+
+$winningBoardScore = getBoardScore($boardsArray, $winningBoard, $winningNumber);
+echo "result: " . $winningBoardScore;
+
+function getBoardScore($boardsArray, $winningBoard, $winningNumber)
+{
+    $result = 0;
+    for ($i = 0; $i < 5; $i++) {
+        for ($j = 0; $j < 5; $j++) {
+            $currentValue = $boardsArray[$winningBoard][$i][$j];
+            if ($currentValue != "x") $result += $currentValue;
+        }
+    }
+    return $result * $winningNumber;
+}
