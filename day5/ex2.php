ec0502b31d6c8416243f845be12be9e98a627620
FedeWade <federico.ballarin.2@studenti.unipd.it>
2022-09-29 23:55:07 +0200

    added day6

--- day5/ex2.php
+++ day5/ex2.php
@@ -10,9 +10,8 @@ while (!feof($inputFile)) {
     $startPoint = explode(",", trim($coordinates[0]));
     $endPoint = explode(",", trim($coordinates[1]));
 
-
     $cloudDirection = getDirection($startPoint, $endPoint);
-    if (($cloudDirection == "horizontal" && $startPoint[0] > $endPoint[0]) || ($cloudDirection == "vertical" && $startPoint[1] > $endPoint[1])) {
+    if (($cloudDirection == "horizontal" && $startPoint[0] > $endPoint[0]) || $startPoint[1] > $endPoint[1]) {
         $temp = array();
         $temp = array_replace($temp, $startPoint);
         $startPoint = array_replace($startPoint, $endPoint);
@@ -20,19 +19,16 @@ while (!feof($inputFile)) {
     }
 
     $cloudLength = ($cloudDirection == "horizontal") ? $endPoint[0] - $startPoint[0] + 1 : $endPoint[1] - $startPoint[1] + 1;
-    if ($cloudDirection != "oblique") {
-        $cloudsMap = drawCloud($cloudsMap, $startPoint, $endPoint, $cloudDirection, $cloudLength);
-    }
+    $cloudsMap = drawCloud($cloudsMap, $startPoint, $endPoint, $cloudDirection, $cloudLength);
 }
 
-$result = 0;
-for ($i = 0; $i < 1000; $i++) {
-    for ($y = 0; $y < 1000; $y++) {
-        if (isset($cloudsMap[$i][$y]) && $cloudsMap[$i][$y] >= 2)
-            $result++;
-    }
-}
-echo "result: " . $result;
+$counter = 0;
+array_walk_recursive($cloudsMap, function ($value, $key) use (&$counter) {
+    if ($value > 1)
+        $counter++;
+}, $counter);
+
+echo "result: " . $counter; //21698 correct result
 
 
 
@@ -40,28 +36,34 @@ function getDirection($startPoint, $endPoint)
 {
     if ($startPoint[0] == $endPoint[0]) return "vertical";
     if ($startPoint[1] == $endPoint[1]) return "horizontal";
-    return "oblique";
+    if (($startPoint[0] < $endPoint[0] && $startPoint[1] < $endPoint[1]) || ($startPoint[0] > $endPoint[0] && $startPoint[1] > $endPoint[1])) return "obliqueRight";
+    if (($startPoint[0] > $endPoint[0] && $startPoint[1] < $endPoint[1]) || $startPoint[0] < $endPoint[0] && $startPoint[1] > $endPoint[1]) return "obliqueLeft";
 }
-
 
 function drawCloud($cloudsMap, $startPoint, $endPoint, $cloudDirection, $cloudLength)
 {
     switch ($cloudDirection) {
         case "vertical":
             for ($i = 0; $i < $cloudLength; $i++) {
-                if (isset($cloudsMap[$startPoint[1] + $i][$startPoint[0]]))
-                    $cloudsMap[$startPoint[1] + $i][$startPoint[0]] += 1;
-                else
-                    $cloudsMap[$startPoint[1] + $i][$startPoint[0]] = 1;
+                $cloudsMap[$startPoint[1] + $i][$startPoint[0]]++;
             }
             break;
 
         case "horizontal":
             for ($i = 0; $i < $cloudLength; $i++) {
-                if (isset($cloudsMap[$startPoint[1]][$startPoint[0] + $i]))
-                    $cloudsMap[$startPoint[1]][$startPoint[0] + $i] += 1;
-                else
-                    $cloudsMap[$startPoint[1]][$startPoint[0] + $i] = 1;
+                $cloudsMap[$startPoint[1]][$startPoint[0] + $i]++;
+            }
+            break;
+
+        case "obliqueRight":
+            for ($i = 0; $i < $cloudLength; $i++) {
+                $cloudsMap[$startPoint[1] + $i][$startPoint[0] + $i]++;
+            }
+            break;
+
+        case "obliqueLeft":
+            for ($i = 0; $i < $cloudLength; $i++) {
+                $cloudsMap[$startPoint[1] + $i][$startPoint[0] - $i]++;
             }
             break;
     }
---
+++ day6/ex2.php
@@ -0,0 +1,19 @@
+<?
+$fileName = __DIR__ . "/input.txt";
+$inputFile = fopen($fileName, "r") or die("unable to open file");
+
+$input = fgets($inputFile);
+$inputList = explode(",", $input);
+
+$fishMap = array(0, 0, 0, 0, 0, 0, 0, 0, 0);
+
+foreach ($inputList as $value) {
+    $fishMap[$value]++;
+}
+
+for ($i = 0; $i < 256; $i++) {
+    $newFish = array_shift($fishMap);
+    $fishMap[6] += $newFish;
+    array_push($fishMap, $newFish);
+}
+echo array_sum($fishMap);
